4a0db3f88b52723fc45f9d8f9aaae62d793ad005
cipriandraghici <cipriandraghici93@gmail.com>
2024-12-10 21:33:18 +0200

    refactoring

--- src/reactjs/hooks/useSignAndSendTransactions.ts
+++ src/reactjs/hooks/useSignAndSendTransactions.ts
@@ -1,14 +1,11 @@
-import { useCallback, useEffect, useMemo, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { PublicClient, Transaction } from 'viem';
 import { TransactionType } from 'types/transaction';
 import { useSignTransactions } from './useSignTransactions';
-import { useWeb3App } from './useWeb3App';
 import { getPublicClient } from '../utils/getPublicClient';
 
 export const useSignAndSendTransactions = () => {
   const publicClientRef = useRef<PublicClient | null>(null);
-
-  const app = useWeb3App();
   const { signTransaction, signTransactions } = useSignTransactions();
 
   const setPublicClient = useCallback(async () => {
@@ -18,15 +15,6 @@ export const useSignAndSendTransactions = () => {
   useEffect(() => {
     setPublicClient();
   }, []);
-
-  const chainId = useMemo(() => app.appKit.getChainId(), [app.appKit]);
-  const client = useMemo(
-    () =>
-      app.config.getClient({
-        chainId: Number(chainId)
-      }),
-    [app.config, chainId]
-  );
 
   const getTransactionByHash = useCallback((hash: `0x${string}`) => {
     return publicClientRef.current?.getTransaction({
@@ -42,7 +30,7 @@ export const useSignAndSendTransactions = () => {
       console.log({ signedTransaction });
       // TODO send signed transaction to the server
     },
-    [client]
+    []
   );
 
   const signAndSendTransactions = useCallback(
