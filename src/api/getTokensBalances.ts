2575b1f8d48e5187e9d26f697daf42874481d9eb
cipriandraghici <cipriandraghici93@gmail.com>
2024-12-23 13:27:58 +0200

    update tokens balances endpoint

--- src/api/getTokensBalances.ts
+++ src/api/getTokensBalances.ts
@@ -3,16 +3,25 @@ import { TokenBalanceDTO } from '../dto/TokenBalance.dto';
 
 export async function getTokensBalances({
   url,
-  userAddress
+  userAddress,
+  chainId
 }: {
   url: string;
-  userAddress?: string;
+  userAddress: string;
+  chainId: string;
 }): Promise<AxiosResponse<TokenBalanceDTO[]>> {
   if (!userAddress) {
     throw new Error('User address is required');
   }
 
-  return await axios.get<TokenBalanceDTO[]>(`/tokens/balances/${userAddress}`, {
-    baseURL: url
-  });
+  if (!chainId) {
+    throw new Error('Chain ID is required');
+  }
+
+  return await axios.get<TokenBalanceDTO[]>(
+    `/tokens/balances/${userAddress}/${chainId}`,
+    {
+      baseURL: url
+    }
+  );
 }
