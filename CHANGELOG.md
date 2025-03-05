b0ca6afe23a3d884e1ed1c4c0fac70e2e68dc000
Ciprian Draghici <cipriandraghici93@gmail.com>
2025-03-05 12:50:10 +0200

    Refetch expired rates (#12)
    * refetch expired rates
    
    * 1.0.7
    
    * changelog update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [[v1.0.7](https://github.com/multiversx/mx-sdk-dapp-liquidity/pull/12)] - 2025-03-05
+
 ## [[v1.0.5](https://github.com/multiversx/mx-sdk-dapp-liquidity/pull/11)] - 2025-03-04
 
 ## [[v1.0.4](https://github.com/multiversx/mx-sdk-dapp-liquidity/pull/10)] - 2025-03-03
--- src/reactjs/queries/useGetRate.mutation.ts
+++ src/reactjs/queries/useGetRate.mutation.ts
@@ -17,6 +17,21 @@ export const useGetRateMutation = () => {
   };
 
   return useMutation({
-    mutationFn
+    mutationFn,
+    onSuccess: (data) => {
+      const currentTime = new Date().getTime();
+
+      if (data.expiresAt && new Date(data.expiresAt).getTime() < currentTime) {
+        console.warn('Retrying due to expired rate');
+        throw new Error('Retrying due to expired rate');
+      }
+
+      return data;
+    },
+    retry: (failureCount, error) => {
+      return (
+        error.message === 'Retrying due to expired rate' && failureCount < 2
+      );
+    }
   });
 };
