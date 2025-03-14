b071f332938282240f553b4094a2d9e68c4505ad
cipriandraghici <cipriandraghici93@gmail.com>
2025-03-14 19:20:55 +0200

    cleanup

--- src/reactjs/index.ts
+++ src/reactjs/index.ts
@@ -4,8 +4,6 @@ export * from './constants';
 export * from './context/Web3AppProvider';
 export * from './context/useWeb3App';
 export * from './context/queryClient';
-export * from './context/FiatProvider';
-export * from './context/useFiat';
 export * from './hooks';
 export * from './init';
 export * from './queries';
