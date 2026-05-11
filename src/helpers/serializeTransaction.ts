6f6484196e70c2bd48cb3a6c6f9bbc7b809ebc82
Radu Mojic <radu.mojic@multiversx.com>
2026-05-11 12:46:39 +0300

    no BigInt precision loss

--- src/helpers/serializeTransaction.ts
+++ src/helpers/serializeTransaction.ts
@@ -2,6 +2,6 @@ import { ServerTransaction } from '../types/transaction';
 
 export function serializeTransaction(transaction: ServerTransaction): string {
   return JSON.stringify(transaction, (_key, value) => {
-    return typeof value === 'bigint' ? Number(value).toString() : value;
+    return typeof value === 'bigint' ? value.toString() : value;
   });
 }
